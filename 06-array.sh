4c3eee63f34afdc9894c74149d3b452d1e32c314
GautamKumar <kodukulagautamkumar@gmail.com>
2025-05-15 22:22:08 +0530

    new file added

--- 06-array.sh
+++ 06-array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-Movies=("Court","HIT3","OG","Thandel")
+Movies=("Court" "HIT3" "OG" "Thandel")
 echo "First movie: ${Movies[0]}"
 echo "Second movie: ${Movies[1]}"
 echo "Third movie: ${Movies[2]}"
