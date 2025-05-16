30593eafe32192b140844b4d476e55934b3521e9
GautamKumar <kodukulagautamkumar@gmail.com>
2025-05-16 16:48:44 +0530

    new file added

--- 09-ec2orroot.sh
+++ 09-ec2orroot.sh
@@ -3,6 +3,7 @@ userid=$(id -u)
 if [ $userid -ne o ]
 then
 echo "Error:: Run script with root access"
+exit 1 #other than '0' any thing can be given
 else
 echo "You are running with root access"
 fi
