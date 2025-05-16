c8bf869e3c491a1f67abfa02f3161e91324ee245
GautamKumar <kodukulagautamkumar@gmail.com>
2025-05-16 16:11:30 +0530

    new file added

---
+++ 07-special-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "To get all variables passed into script: $@"
+echo "Number of variable: $#"
+echo "script name: $0"
+echo "Current directory: $PWD"
+echo "Home directory: $HOME"
+echo "PID of current script: $$"
+echo "Last command in background: $!"
