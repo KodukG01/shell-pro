8b8d31e5f73c281baff78c1a76ebfc61b4f98ee8
GautamKumar <kodukulagautamkumar@gmail.com>
2025-05-23 20:41:35 +0530

    new file added

--- delete-logs.sh
+++ delete-logs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -6,8 +7,11 @@ Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME.log
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SOURCE_DIR=/home/ec2-user/app-logs
+
+mkdir -p $LOGS_FOLDER
+
 if [ $USERID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
@@ -26,9 +30,8 @@ VALIDATE(){
         exit 1
     fi
 }
-mkdir -p $LOGS_FOLDER
 
-echo "Script started executing at $(date) | tee -a $LOG_FILE  
+echo "Script started executing at $(date)" | tee -a $LOG_FILE
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 
