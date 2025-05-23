b363f8d6908cc58805a1b2b246a04464a9387d91
GautamKumar <kodukulagautamkumar@gmail.com>
2025-05-23 22:27:38 +0530

    new file added

--- backup.sh
+++ backup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 USERID=$(id -u)
 SOURCE_DIR=$1
 DEST_DIR=$2
-DAYS=${3:-14}
+DAYS=${3:-14} # if DAYS are provided that will be considered, otherwise default 14 days
+
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
@@ -10,6 +12,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
 # validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
@@ -30,53 +33,55 @@ check_root(){
         echo "You are running with root access" 
     fi
 }
+
 check_root
 mkdir -p $LOGS_FOLDER
 
-usage(){
-    echo -e "$R USAGE:: $N sh backup.sh <source-dir> <destination-dir> <days(optional)"
+USAGE(){
+    echo -e "$R USAGE:: $N sh 20-backup.sh <source-dir> <destination-dir> <days(optional)>"
     exit 1
 }
 
 if [ $# -lt 2 ]
 then
-    usage
+    USAGE
 fi
 
 if [ ! -d $SOURCE_DIR ]
 then
-echo -e "$R Source Directory $SOURCE_DIR does not exist, please check $N"
-exit 1
+    echo -e "$R Source Directory $SOURCE_DIR does not exist. Please check $N"
+    exit 1
 fi
 
-if [ ! -d  $DEST_DIR ]
+if [ ! -d $DEST_DIR ]
 then
-echo -e "$R Destination Directory $DEST_DIR does not exist, please check $N"
-exit 1
+    echo -e "$R Destination Directory $DEST_DIR does not exist. Please check $N"
+    exit 1
 fi
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
-if [ ! -z $FILES ]
+if [ ! -z "$FILES" ]
 then
     echo "Files to zip are: $FILES"
     TIMESTAMP=$(date +%F-%H-%M-%S)
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
-    echo $FILES | zip -@ $ZIP_FILE
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
 
     if [ -f $ZIP_FILE ]
     then
-        echo -e "Successfully created zip file"
+        echo -e "Successfully created Zip file"
+
         while IFS= read -r filepath
         do
-        echo "Deleting file: $filepath" | tee -a $LOG_FILE
-        rm -rf $filepath
-        done <<< $FILES_TO_DELETE
-
+            echo "Deleting file: $filepath" | tee -a $LOG_FILE
+            rm -rf $filepath
+        done <<< $FILES
+        echo -e "Log files older than $DAYS from source directory removed ... $G SUCCESS $N"
     else
-        echo -e "No zip file"
-    fi    
+        echo -e "Zip file creation ... $R FAILURE $N"
+        exit 1
+    fi
 else
-echo "No files found older than 14 days ..... $Y Skipping $N"
+    echo -e "No log files found older than 14 days ... $Y SKIPPING $N"
 fi
-
