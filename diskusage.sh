0be32d418079d03e756588381c90c392deeb4c8e
GautamKumar <kodukulagautamkumar@gmail.com>
2025-05-26 14:25:00 +0530

    new file added

---
+++ diskusage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=1 #In project it will be 75%
+
+while read -r line
+do
+    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $6F}')
+    echo "$PARTITION: $USAGE"
+
+done <<< $DISK_USAGE
