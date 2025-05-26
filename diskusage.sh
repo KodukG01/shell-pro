99d858576bc2dd7ab97b14e10cffb53441c9dd66
GautamKumar <kodukulagautamkumar@gmail.com>
2025-05-26 14:56:00 +0530

    new file added

--- diskusage.sh
+++ diskusage.sh
@@ -2,18 +2,19 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 #In project it will be 75%
-
-while read -r line
+MSG=""
+IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+while IFS= read -r line
 do
     USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7F}')
     echo "$PARTITION: $USAGE"
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        MSG+="High Disk Usage on $PARTITION: $USAGE"
+        MSG+="High Disk Usage on $PARTITION: $USAGE % <br>"
     fi
 
 
 done <<< $DISK_USAGE
 
-echo $MSG   
+sh maail.sh "DevOps Team" "High Disk Usage" $IP $MSG "kgautamkumar20052@gmail.com" "ALERT-High Disk Usage"
